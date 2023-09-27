d76b3d6309c007485a62018263e818e1c3d58e9c
Caleb Courier <caleb.courier@gmail.com>
2023-09-26 18:03:01 -0700

    standardize names

--- src/modules/otel_meter/__init__.py
+++ src/modules/otel_meter/__init__.py
@@ -1,5 +1,6 @@
+import os
 from opentelemetry import metrics
 
-
+service_name = os.environ.get('OTEL_SERVICE_NAME', 'guardrails-api')
 provider = metrics.get_meter_provider()
-otel_meter = provider.get_meter(__name__)
+otel_meter = provider.get_meter(service_name)
--- src/modules/otel_tracer/__init__.py
+++ src/modules/otel_tracer/__init__.py
@@ -7,8 +7,8 @@ from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExport
 # configure
 OTEL_EXPORTER_OTLP_ENDPOINT = os.environ.get('OTEL_EXPORTER_OTLP_ENDPOINT', 'http://otel-collector:4317')
 provider = trace.get_tracer_provider()
-print("trace provider:", provider)
 processor = BatchSpanProcessor(OTLPSpanExporter(endpoint=OTEL_EXPORTER_OTLP_ENDPOINT, insecure=True))
 provider.add_span_processor(processor)
 
-otel_tracer = trace.get_tracer(__name__)
+service_name = os.environ.get('OTEL_SERVICE_NAME', 'guardrails-api')
+otel_tracer = trace.get_tracer(service_name)
