79898c180bb4d49a84152e09b5400523a73b2ff0
Caleb Courier <caleb.courier@gmail.com>
2023-11-13 10:53:29 -0600

    get collector working as extension

--- buildspecs/build.sh
+++ buildspecs/build.sh
@@ -14,6 +14,15 @@ ecrImageUrl="${ecrEndpoint}/${repoName}";
 echo "Building api client..."
 bash build-sdk.sh
 
+# Building OTEL Collector extension
+if [ -d "opentelemetry-lambda-layer" ]; then
+  rm -rf opentelemetry-lambda-layer
+fi
+
+curl $(aws lambda get-layer-version-by-arn --arn arn:aws:lambda:us-east-1:184161586896:layer:opentelemetry-collector-arm64-0_2_0:1 --query 'Content.Location' --output text) --output otel-collector.zip
+unzip otel-collector.zip -d ./opentelemetry-lambda-layer
+rm otel-collector.zip
+
 echo "Performing docker build"
 docker login -u AWS -p $(aws ecr get-login-password --region $region) $ecrEndpoint;
 
@@ -26,8 +35,7 @@ docker buildx build \
   || exit 1;
 
 docker image tag "$imageName:$commitSha" "$ecrImageUrl:$commitSha";
-docker image tag "$imageName:$version" "$ecrImageUrl:$version";
 docker image tag "$imageName:latest" "$ecrImageUrl:latest";
 
-echo "Publishing to ECR"
+# echo "Publishing to ECR"
 docker push $ecrImageUrl --all-tags;
--- src/modules/otel_tracer/__init__.py
+++ src/modules/otel_tracer/__init__.py
@@ -5,9 +5,16 @@ from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExport
 
 
 # configure
-OTEL_EXPORTER_OTLP_ENDPOINT = os.environ.get('OTEL_EXPORTER_OTLP_ENDPOINT', 'http://otel-collector:4317')
+OTEL_EXPORTER_OTLP_ENDPOINT = os.environ.get('OTEL_EXPORTER_OTLP_ENDPOINT', 'http://localhost:4317') # 'http://otel-collector:4317')
+OTEL_TRACE_SINK = os.environ.get('OTEL_TRACE_SINK')
+endpoint = OTEL_TRACE_SINK or OTEL_EXPORTER_OTLP_ENDPOINT
+print("OTEL_EXPORTER_OTLP_ENDPOINT: ", OTEL_EXPORTER_OTLP_ENDPOINT)
+print("OTEL_TRACE_SINK: ", OTEL_TRACE_SINK)
+print("endpoint: ", endpoint)
+insecure = not endpoint.startswith('https://')
 provider = trace.get_tracer_provider()
-processor = BatchSpanProcessor(OTLPSpanExporter(endpoint=OTEL_EXPORTER_OTLP_ENDPOINT, insecure=True))
+processor = BatchSpanProcessor(OTLPSpanExporter(endpoint=endpoint, insecure=insecure))
+print("exporter endpoint: ", processor.span_exporter._endpoint)
 provider.add_span_processor(processor)
 
 service_name = os.environ.get('OTEL_SERVICE_NAME', 'guardrails-api')
