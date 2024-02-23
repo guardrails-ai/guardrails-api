22bffec5ef6c6cb2c1d4bb8e1666df455e6df6d2
Caleb Courier <caleb.courier@gmail.com>
2024-02-22 18:37:41 -0600

    turn off auto-instr

--- prod-build.sh
+++ prod-build.sh
@@ -4,15 +4,15 @@ echo "Building api client..."
 # bash build-sdk.sh
 
 # Copy Lambda Layer and convert to extension
-if [ ! -d "opentelemetry-lambda-layer" ]; then
-  curl $(aws lambda get-layer-version-by-arn --arn arn:aws:lambda:us-east-1:184161586896:layer:opentelemetry-collector-arm64-0_2_0:1 --query 'Content.Location' --output text) --output otel-collector.zip
-  unzip otel-collector.zip -d ./opentelemetry-lambda-layer
-  rm otel-collector.zip
-fi
+# if [ ! -d "opentelemetry-lambda-layer" ]; then
+#   curl $(aws lambda get-layer-version-by-arn --arn arn:aws:lambda:us-east-1:184161586896:layer:opentelemetry-collector-arm64-0_2_0:1 --query 'Content.Location' --output text) --output otel-collector.zip
+#   unzip otel-collector.zip -d ./opentelemetry-lambda-layer
+#   rm otel-collector.zip
+# fi
 
 docker build \
     -f Dockerfile.prod \
     --progress=plain \
+    --build-arg CACHEBUST="$(date)" \
+    --no-cache \
     -t "guardrails-api:prod" .;
-    # --no-cache \
-    # --build-arg CACHEBUST="$(date)" \
--- src/modules/otel_meter/__init__.py
+++ src/modules/otel_meter/__init__.py
@@ -1,8 +1,8 @@
 # !!! UPDATE ME !!!
 # See Tracer setup
-import os
-from opentelemetry import metrics
+# import os
+# from opentelemetry import metrics
 
-service_name = os.environ.get("OTEL_SERVICE_NAME", "guardrails-api")
-provider = metrics.get_meter_provider()
-otel_meter = provider.get_meter(service_name)
+# service_name = os.environ.get("OTEL_SERVICE_NAME", "guardrails-api")
+# provider = metrics.get_meter_provider()
+# otel_meter = provider.get_meter(service_name)
