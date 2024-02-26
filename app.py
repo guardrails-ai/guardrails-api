fd6c28fb048f80fbeee32227f3131f544ed08b91
Caleb Courier <caleb.courier@gmail.com>
2024-02-26 14:11:14 -0600

    additional config updates for ssl

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
 import os
 from flask import Flask
 from flask_cors import CORS
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 
 def create_app():
     app = Flask(__name__)
-    host = os.environ.get("SELF_ENDPOINT", "http://localhost:8000")
-    if host.startswith("https://"):
-        from flask_talisman import Talisman
-        Talisman(app)
     
+    app.config['APPLICATION_ROOT'] = '/'
+    app.config['PREFERRED_URL_SCHEME'] = 'https'
+    app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_port=1)
+
     CORS(app)
+    # host = os.environ.get("SELF_ENDPOINT", "http://localhost:8000")
+    # if host.startswith("https://"):
+    #     from flask_talisman import Talisman
+    #     Talisman(app)
+    
+    
 
     from src.clients.postgres_client import PostgresClient
 
