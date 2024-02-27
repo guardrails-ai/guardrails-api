d6013e8d6b35a4bacb12709e3dd6064c93787c73
Caleb Courier <caleb.courier@gmail.com>
2024-02-27 10:31:09 -0600

    try reverse proxy again

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import os
 from flask import Flask, request, Response
 from flask_cors import CORS
-# from werkzeug.middleware.proxy_fix import ProxyFix
+from werkzeug.middleware.proxy_fix import ProxyFix
 from urllib.parse import urlparse
-# from flask_talisman import Talisman
 
 
 class ReverseProxied(object):
@@ -31,58 +30,18 @@ def create_app():
         print(f"Responding to request with status: {response.status}")
         return response
 
+    app.config['APPLICATION_ROOT'] = '/'
     app.config["PREFERRED_URL_SCHEME"] = "https"
-    ReverseProxied(app)
+    app.wsgi_app = ReverseProxied(app.wsgi_app)
     CORS(app)
 
-    # alt_scheme = 'https'
-    # alt_port = 80
-    # if url.scheme == 'https':
-    #     alt_scheme = 'http'
-    #     alt_port = 443
-
-    # alt_endpoint = f"{alt_scheme}://{url.hostname}"
-    # alt_endpoint_w_port = f"{alt_scheme}://{url.hostname}:{alt_port}"
-    # Talisman(
-    #     app,
-    #     force_https=False,
-    #     content_security_policy={
-    #         "default-src": [
-    #             "'self'",
-    #             self_endpoint,
-    #             alt_endpoint,
-    #             alt_endpoint_w_port,
-    #             "https://unpkg.com",
-    #             "http://www.w3.org"
-    #         ],
-    #         "script-src": [
-    #             "'self'",
-    #             self_endpoint,
-    #             alt_endpoint,
-    #             alt_endpoint_w_port,
-    #             "https://unpkg.com",
-    #             "http://www.w3.org"
-    #         ],
-    #         "img-src": [
-    #             "'self'",
-    #             "data:",
-    #             self_endpoint,
-    #             alt_endpoint,
-    #             alt_endpoint_w_port,
-    #             "https://unpkg.com",
-    #             "http://www.w3.org"
-    #         ]
-    #     },
-    #     content_security_policy_nonce_in=["script-src"]
-    # )
-    # app.wsgi_app = ProxyFix(
-    #     app.wsgi_app,
-    #     x_for=0,
-    #     x_proto=0,
-    #     # x_host=1,
-    #     # x_port=1,
-    #     # x_prefix=1
-    # )
+    app.wsgi_app = ProxyFix(
+        app.wsgi_app,
+        x_for=1,
+        x_proto=1,
+        x_host=1,
+        x_port=1
+    )
 
     from src.clients.postgres_client import PostgresClient
 
