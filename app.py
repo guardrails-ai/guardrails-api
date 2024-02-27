21d9f2ee01e1f42b8922f4cce37b85c416edc9ca
Caleb Courier <caleb.courier@gmail.com>
2024-02-27 08:38:51 -0600

    try tweaking header settings

--- app.py
+++ app.py
@@ -50,11 +50,11 @@ def create_app():
     )
     app.wsgi_app = ProxyFix(
         app.wsgi_app,
-        x_for=1,
-        x_proto=1,
-        x_host=1,
-        x_port=1,
-        x_prefix=1
+        x_for=0,
+        x_proto=0,
+        # x_host=1,
+        # x_port=1,
+        # x_prefix=1
     )
     
     
