26267fd5d089babdaf4fb9a319e91c56c6818efc
Caleb Courier <caleb.courier@gmail.com>
2024-03-20 11:45:48 -0500

    cleanup

--- enterprise-run.sh
+++ enterprise-run.sh
@@ -1,4 +1,3 @@
 docker stop guardrails-api-enterprise || true
 docker rm guardrails-api-enterprise || true
 docker run -p 8000:8000 --env-file local.env --name guardrails-api-enterprise -it guardrails-api-enterprise:latest
-# docker run -p 8000:8000 --env-file local.env --name guardrails-api-enterprise -it graas-heavy:latest
