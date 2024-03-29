b5313b863407106fda5f9055259dafeb18b0a985
Caleb Courier <caleb.courier@gmail.com>
2024-03-29 11:14:08 -0500

    revert script change

--- dev-run.sh
+++ dev-run.sh
@@ -1,4 +1,3 @@
 docker stop guardrails-api-dev || true
 docker rm guardrails-api-dev || true
 docker run -p 8000:8000 --env-file local.env --name guardrails-api-dev -it guardrails-api:dev
-# docker run -p 8000:8000 --env-file local.env --name guardrails-api-dev -it graas-heavy:latest
