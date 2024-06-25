bb20bd344cc6d9f948e5061eababa5e5fe3ff504
Nicholas Chen <nichwch@gmail.com>
2024-06-25 14:44:50 -0400

    move config into sample-config

--- guardrails_api/config.py
+++ guardrails_api/config.py
@@ -7,25 +7,5 @@ undefined. Otherwise, a postgres instance will be started
 and guards will be persisted into postgres. In that case,
 these guards will not be initialized.
 """
-# from guardrails_hub import AtomicFactuality  # noqa
+
 from guardrails import Guard
-from guardrails.hub import (
-    DetectPII,
-    CompetitorCheck
-)
-
-
-no_guards = Guard()
-no_guards.name = "No Guards"
-
-output_guard = Guard()
-output_guard.name = "Output Guard"
-output_guard.use_many(
-    DetectPII(
-        pii_entities='pii'
-    ),
-    CompetitorCheck(
-        competitors=['OpenAI', 'Anthropic']
-    )
-)
-
