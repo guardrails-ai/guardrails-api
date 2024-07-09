eb05c63f5a73bf2821c9e9b0d636f9a7226cabb1
David Tam <david@guardrailsai.com>
2024-07-09 15:10:39 -0700

    remove unecessary openai error handling code now we have the base handler

--- guardrails_api/blueprints/guards.py
+++ guardrails_api/blueprints/guards.py
@@ -191,26 +191,19 @@ def openai_v1_chat_completions(guard_name: str):
         pass
 
     if not stream:
-        try:
-            validation_outcome: ValidationOutcome = guard(
-                # todo make this come from the guard struct?
-                # currently we dont support .configure
-                num_reasks=0,
-                **payload,
-            )
-            llm_response = guard.history[-1].iterations[-1].outputs.llm_response_info
-            result = outcome_to_chat_completion(
-                validation_outcome=validation_outcome,
-                llm_response=llm_response,
-                has_tool_gd_tool_call=has_tool_gd_tool_call,
-            )
-            return result
-        except Exception as e:
-            raise HttpError(
-                status=400,
-                message="BadRequest",
-                cause=(str(e)),
-            )
+        validation_outcome: ValidationOutcome = guard(
+            # todo make this come from the guard struct?
+            # currently we dont support .configure
+            num_reasks=0,
+            **payload,
+        )
+        llm_response = guard.history[-1].iterations[-1].outputs.llm_response_info
+        result = outcome_to_chat_completion(
+            validation_outcome=validation_outcome,
+            llm_response=llm_response,
+            has_tool_gd_tool_call=has_tool_gd_tool_call,
+        )
+        return result
 
     else:
         # need to return validated chunks that look identical to openai's
