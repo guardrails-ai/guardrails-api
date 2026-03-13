623239100e69ba05894b057d43cdf3cd63295e38
Caleb Courier <caleb@guardrailsai.com>
2026-03-13 15:06:57 -0500

    verify check

--- integration_tests/check.sh
+++ integration_tests/check.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+up="F"
 for i in {1..30}; do
 if docker exec guardrails-server curl -s http://localhost:8000/; then
     echo "Server is up!"
+    up="T"
     break
 fi
 echo "Waiting for server..."
 sleep 5
 done
+
+if [ "T" != "${up}" ];
+    then
+        echo "Server never started!"
+        exit 1
+fi
