7707b5acf58c171e9134dc7c7f24c1a0aca33f26
Caleb Courier <caleb@guardrailsai.com>
2026-04-03 12:26:02 -0500

    use secret mounts instead of build arg for gr token

--- integration_tests/build.sh
+++ integration_tests/build.sh
@@ -5,7 +5,7 @@ docker buildx build \
     --platform linux/arm64 \
     -f "./integration_tests/Dockerfile.w_config" \
     -t "guardrails-api:integration-tests" \
-    --build-arg GUARDRAILS_TOKEN="$GUARDRAILS_TOKEN" \
+    --secret id=GUARDRAILS_TOKEN \
     --progress plain \
     --load . \
     || exit 1
